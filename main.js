e1ad271d57ddd278903075a02c267e1005b93774
Sisira Mandapaka <sisiramandapaka@gmail.com>
2022-02-15 17:45:45 -0500

    added the javascript

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-function buttonClicked() {
-    let newText = "Button was clicked!"; 
-    // we need to select the button-div (using its id) 
-    let buttonDiv = document.getElementById("button-div"); 
-    // then we buttonDiv's content
-    buttonDiv.innerHTML = newText; 
+function circle1Clicked() {
+    let displayCount = document.getElementById("numClicks"); 
+
+    let newCount = parseInt(displayCount.innerHTML);
+    newCount++;
+
+    displayCount.innerHTML = newCount; 
+}
+
+function circle2Mouseover() {
+    let circle2 = document.getElementById("circle2");
+    circle2.style.strokeWidth = 3;
+}
+
+function circle2Mouseout() {
+    let circle2 = document.getElementById("circle2");
+    circle2.style.strokeWidth = 0;
+}
+
+function circle3Clicked(){
+    let circle3 = document.getElementById("circle3");
+    let opacity = circle3.style.opacity;
+
+    if(opacity == 1){
+        opacity = 0;
+    }
+    else {
+        opacity = 1;
+    }
+
+    circle3.style.opacity = opacity;
 }
